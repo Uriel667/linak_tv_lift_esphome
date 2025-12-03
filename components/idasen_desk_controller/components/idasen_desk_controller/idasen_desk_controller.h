0e06a34090f69ebc2e503c9b7d4ddfce0930ae68
Uriel667 <hjengelbrecht1@gmail.com>
2025-12-03 14:52:04 +0100

    Create idasen_desk_controller.h

---
+++ components/idasen_desk_controller/components/idasen_desk_controller/idasen_desk_controller.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/ble_client/ble_client.h"
+#include "esphome/components/esp32_ble_tracker/esp32_ble_tracker.h"
+#include "esphome/components/cover/cover.h"
+#include "esphome/components/sensor/sensor.h"
+
+namespace esphome {
+namespace idasen_desk_controller {
+
+// Linak 128-bit UUIDs
+static const esp32_ble::ESPBTUUID CONTROL_SERVICE_UUID = esp32_ble::ESPBTUUID::from_raw("99FA0001-338A-1024-8A49-009C0215F78A");
+static const esp32_ble::ESPBTUUID CONTROL_CHARACTERISTIC_UUID = esp32_ble::ESPBTUUID::from_raw("99FA0002-338A-1024-8A49-009C0215F78A");
+
+class IdasenDeskController : public Component, public ble_client::BLEClientNode {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) override;
+  void set_ble_client_parent(ble_client::BLEClient *parent) { parent_ = parent; }
+  void set_height_sensor(sensor::Sensor *sensor) { height_sensor_ = sensor; }
+
+  void move_up();
+  void move_down();
+  void stop_move();
+
+ protected:
+  ble_client::BLEClient *parent_;
+  sensor::Sensor *height_sensor_{nullptr};
+  uint16_t char_handle_{0};
+};
+
+class IdasenDeskControllerCover : public cover::Cover, public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
+  void set_idasen_desk_controller(IdasenDeskController *controller) { controller_ = controller; }
+  cover::CoverTraits get_traits() override;
+
+ protected:
+  void control(const cover::CoverCall &call) override;
+  IdasenDeskController *controller_;
+};
+
+}  // namespace idasen_desk_controller
+}  // namespace esphome
