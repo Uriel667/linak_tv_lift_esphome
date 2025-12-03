b8293b5d082cab9c439882c7308538529965bdb7
Uriel667 <hjengelbrecht1@gmail.com>
2025-12-03 15:12:20 +0100

    Update sensor.py

--- components/idasen_desk_controller/sensor.py
+++ components/idasen_desk_controller/sensor.py
@@ -2,17 +2,17 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
 from esphome.const import CONF_ID, UNIT_CENTIMETER, ICON_ARROW_EXPAND_VERTICAL
-from . import IdasenDeskController
+from . import LinakTvLift
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     unit_of_measurement=UNIT_CENTIMETER,
     icon=ICON_ARROW_EXPAND_VERTICAL,
     accuracy_decimals=0
 ).extend({
-    cv.GenerateID('idasen_desk_controller_id'): cv.use_id(IdasenDeskController),
+    cv.GenerateID('linak_tv_lift_id'): cv.use_id(LinakTvLift),
 })
 
 def to_code(config):
-    parent = yield cg.get_variable(config['idasen_desk_controller_id'])
+    parent = yield cg.get_variable(config['linak_tv_lift_id'])
     var = yield sensor.new_sensor(config)
     cg.add(parent.set_height_sensor(var))
