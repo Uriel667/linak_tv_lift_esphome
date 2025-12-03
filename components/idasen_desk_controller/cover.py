87f295868994375852bd47d0034fe512da17b174
Uriel667 <hjengelbrecht1@gmail.com>
2025-12-03 15:06:43 +0100

    Update cover.py

--- components/idasen_desk_controller/cover.py
+++ components/idasen_desk_controller/cover.py
@@ -2,19 +2,18 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import cover
 from esphome.const import CONF_ID
-from . import IdasenDeskController, idasen_desk_controller_ns
+from . import LinakTvLift, linak_tv_lift_ns
 
-IdasenDeskControllerCover = idasen_desk_controller_ns.class_('IdasenDeskControllerCover', cover.Cover, cg.Component)
+LinakTvLiftCover = linak_tv_lift_ns.class_('LinakTvLiftCover', cover.Cover, cg.Component)
 
-# 2025 Fix: Call cover_schema function before extending
-CONFIG_SCHEMA = cover.cover_schema(IdasenDeskControllerCover).extend({
-    cv.GenerateID(): cv.declare_id(IdasenDeskControllerCover),
-    cv.GenerateID('idasen_desk_controller_id'): cv.use_id(IdasenDeskController),
+CONFIG_SCHEMA = cover.cover_schema(LinakTvLiftCover).extend({
+    cv.GenerateID(): cv.declare_id(LinakTvLiftCover),
+    cv.GenerateID('linak_tv_lift_id'): cv.use_id(LinakTvLift),
 })
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
     yield cover.register_cover(var, config)
-    parent = yield cg.get_variable(config['idasen_desk_controller_id'])
-    cg.add(var.set_idasen_desk_controller(parent))
+    parent = yield cg.get_variable(config['linak_tv_lift_id'])
+    cg.add(var.set_linak_tv_lift(parent))
